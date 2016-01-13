56a8e0aa7c2fd16804b04ba872738b6f03dc2525
Ómar Kjartan Yasin <omar@projector.com>
2016-01-12 22:33:23 -0800

    Refactor the client to use the new en_server http2 connection.

--- src/http2_client.erl
+++ src/http2_client.erl
@@ -7,7 +7,9 @@
 -export([start_link/1]).
 
 -export([send_request/4,
-	 send_request_sync/4]).
+	 google/1]).
+
+-export([frame/2]).
 
 -export([init/1,
 	 handle_event/3,
@@ -16,27 +18,16 @@
 	 code_change/4,
 	 terminate/3]).
 
--export([connected/2]).
-
--record(parser, {
-	  wfh :: frame_header()|undefined,
-	  wfp = <<>> :: binary(),
-	  frames = [] :: [frame()]
-	 }).
-
--record(stream, {
-	  sender :: pid(),
-	  frames :: [frame()]
-	 }).
+-export([waiting_for_server_settings/2,
+	 waiting_for_server_ack/2,
+	 connected/2]).
 
 -record(state, {
-	  connection,
+	  conn,
 	  client_settings,
 	  server_settings,
-	  nas = 1 :: pos_integer(),
-	  parser = #parser{} :: #parser{},
-	  streams = dict:new() :: dict:dict(),
-	  ping_time :: non_neg_integer()
+	  ack = false :: boolean(),
+	  streams = dict:new() :: dict:dict()
 	 }).
 
 -type option() :: {host, string()} |
@@ -48,51 +39,55 @@
                         {ok, pid()} |
                         ignore |
                         {error, term()}.
-start_link(Options) when is_list(Options) ->
-    Host = proplists:get_value(host, Options),
-    Port = proplists:get_value(port, Options),
-    PingTime = proplists:get_value(ping_time, Options, 5000),
-    {Transport, ClientOptions} = client_options(Options),
-    case Transport:connect(Host, Port, ClientOptions) of
-	{ok, Socket} ->
-	    ConnectionState = #connection_state{socket = {Transport, Socket}},
-	    handshake_connection(#state{connection=ConnectionState,
-					ping_time=PingTime});
-	{error, timeout} ->
-	    {error, timeout};
-	Error ->
-	    Error
-    end.
+start_link(Options) ->
+    gen_fsm:start_link(?MODULE, [Options], []).
+
+frame(Pid, Frame) ->
+    gen_fsm:send_event(Pid, {frame, Frame}).
+
+google(Pid) ->
+    send_request(Pid, [{<<":path">>, <<"/">>}], <<>>, []).
 
 send_request(Pid, Headers, Body, Options) ->
     gen_fsm:send_event(Pid, {send_request, Headers, Body, Options}).
 
-send_request_sync(Pid, Headers, Body, Timeout) ->
-    Sender = self(),
-    gen_fsm:send_event(Pid, {send_request, Headers, Body, [{sender, Sender}]}),
-    receive
-	{http2, {frame, Frame}} ->
-	    Frame
-    after Timeout ->
-	    timeout
+init([Options]) ->
+    case http2_conn:start_link(Options++[{client, self()}]) of
+	{ok, Pid} ->
+	    http2_conn:send_presamble(Pid),
+	    Raw = http2_frame_settings:get_frame(#settings{}, #settings{}),
+	    CS = #connection_state{socket=Pid,
+				   next_available_stream_id=1},
+	    send_raw(Raw, CS),
+	    {ok, waiting_for_server_settings,
+	     #state{conn=CS, client_settings=#settings{}}};
+	Error ->
+	    Error
     end.
 
-init([#state{connection=#connection_state{socket={Transport, Socket}}}=State]) ->
-    % The connection has been successfully handshaked before the process is
-    % started.
-    Transport:setopts(Socket, [{active, true}]),
-    {ok, connected, State}.
+waiting_for_server_settings({frame, {#frame_header{type=?SETTINGS},
+				     ServerSettings}}, State) ->
+    % Maybe set a timer and wait for the ack. If it does not come in time,
+    % shutdown?
+    {next_state, waiting_for_server_ack,
+     State#state{server_settings=ServerSettings}}.
 
-connected({send_request, Headers, Body, Options},
-	  #state{nas=Nas, connection=Connection, streams=Streams}=State) ->
-    Matcher = proplists:get_value(matcher, Options),
-    Stream = #stream{sender=proplists:get_value(sender, Options)},
-    Streams1 = dict:store(Nas, Stream, Streams),
-    {HeaderFrame, Connection1} = frame_headers(Headers, Nas, Matcher, Connection),
-    DataFrame = frame_body(Body, Nas, Connection),
-    send_frames([HeaderFrame|DataFrame], Connection),
-    {next_state, connected, State#state{connection=Connection1, nas=Nas+2,
-					streams = Streams1}};
+waiting_for_server_ack({frame, {#frame_header{type=?SETTINGS}=FrameHeader,
+			        _}}, State) ->
+    Ack = ?IS_FLAG(FrameHeader#frame_header.flags, ?FLAG_ACK),
+    {next_state, connected, State#state{ack=Ack}}.
+
+connected({send_request, Headers, Body, Options}, #state{conn=CS}=State) ->
+    {FramedHeaders, CS1} = frame_headers(Headers, Options, CS),
+    FramedBody = frame_body(Body, CS1),
+    send_frames([FramedHeaders|FramedBody], CS),
+    {next_state, connected, increment_nasi(State#state{conn=CS})};
+
+connected({frame, {#frame_header{type=?GOAWAY}, _}}, State) ->
+    {stop, normal, State};
+connected({frame, {_FrameHeader, _Payload}=Frame}, State) ->
+    lager:debug("Unhandled frame: ~p", [Frame]),
+    {next_state, connected, State};
 connected(_Event, State) ->
     {next_state, connected, State}.
 
@@ -102,13 +97,8 @@ handle_event(_Event, FsmState, State) ->
 handle_sync_event(_Event, _From, FsmState, State) ->
     {next_state, FsmState, State}.
 
-handle_info({_Transport, _Socket, Data}, connected,
-	    #state{parser=Parser, connection=Connection,
-		   streams=Streams}=State) ->
-    {Frames, Parser1} = process_binary(Data, Parser),
-    {Streams1, Connection1} = notify_senders(Frames, Streams, Connection),
-    {next_state, connected, State#state{parser=Parser1, streams=Streams1,
-					connection=Connection1}}.
+handle_info(_, FsmState, State) ->
+    {next_state, FsmState, State}.
 
 code_change(_OldVsn, StateName, State, _Extra) ->
     {ok, StateName, State}.
@@ -118,118 +108,30 @@ terminate(normal, _StateName, _State) ->
 terminate(_Reason, _StateName, _State) ->
     lager:debug("terminate reason: ~p~n", [_Reason]).
 
-% Internal
-notify_senders([], Streams, Connection) ->
-    {Streams, Connection};
-notify_senders([{#frame_header{stream_id=SID}, _}=Frame|Frames], Streams,
-	       Connection) ->
-    case dict:find(SID, Streams) of
-	{ok, #stream{sender=_}} ->
-	    {_Parsed, Connection1} = parse_frame(Frame, Connection),
-	    %Pid ! {http2, {frame, Parsed}},
-	    Streams1 = remove_stream(Frame, Streams),
-	    notify_senders(Frames, Streams1, Connection1);
-	error ->
-	    notify_senders(Frames, Streams, Connection)
-    end.
+%% Internal
+increment_nasi(#state{conn=#connection_state{next_available_stream_id=NASI}=CS}=State) ->
+    State#state{conn=CS#connection_state{next_available_stream_id=NASI+2}}.
 
-parse_frame({#frame_header{type = ?DATA}, #data{data=Data}}, Connection) ->
-    lager:debug("Data frame ~p", [Data]),
-    {{data, Data}, Connection};
-parse_frame({#frame_header{type = ?HEADERS}, #headers{block_fragment=BF}},
-	    #connection_state{decode_context=Ctx}=CS) ->
-    lager:debug("Body ~p", [BF]),
-    {Headers, _Ctx1} = hpack:decode(BF, Ctx),
-    lager:debug("Body ~p", [Headers]),
-    {{headers, []}, CS};
-parse_frame(Frame, Connection) ->
-    lager:debug("Some frame ~p", [Frame]),
-    {Frame, Connection}.
+send_frames(Frame, #connection_state{socket=Pid}) ->
+    http2_conn:send_frames(Pid, Frame).
 
-remove_stream(#frame_header{flags = 1, stream_id = SID}, Streams) ->
-    dict:erase(SID, Streams);
-remove_stream(_, Streams) ->
-    Streams.
+send_raw(Raw, #connection_state{socket=Pid}) ->
+    http2_conn:send_raw(Pid, Raw).
 
-frame_body(Body, Nas, #connection_state{send_settings=SS}) ->
-    http2_frame_data:to_frames(Nas, Body, SS).
+frame_headers(Headers, Options,
+	      #connection_state{encode_context=EC,
+				next_available_stream_id=NASI}=CS) ->
+    case proplists:get_value(matcher, Options) of
+	undefined ->
+	    {HeaderFrame, EC1} = http2_frame_headers:to_frame(NASI, Headers,
+							      EC),
+	    {HeaderFrame, CS#connection_state{encode_context=EC1}};
+	Matcher ->
+	    {HeaderFrame, EC1} = http2_frame_headers:to_frame(NASI, Headers,
+							      Matcher, EC),
+	    {HeaderFrame, CS#connection_state{encode_context=EC1}}
+end.
 
-frame_headers(Headers, Nas, Matcher,
-	      #connection_state{encode_context=EC}=Connection) ->
-    {HeaderFrame, EC1} = http2_frame_headers:to_frame(Nas, Headers, Matcher, EC),
-    {HeaderFrame, Connection#connection_state{encode_context=EC1}}.
-
-send_frames(Frames, #connection_state{socket={Transport, Socket}}) ->
-    [Transport:send(Socket, http2_frame:to_binary(F)) || F <- Frames].
-
-handshake_connection(
-  #state{connection=#connection_state{socket={Transport, Socket}}}=State) ->
-    ok = Transport:send(Socket, <<?PREAMBLE>>),
-    ClientSettings = #settings{},
-    http2_frame_settings:send({Transport, Socket}, #settings{}, ClientSettings),
-    case http2_frame:read({Transport, Socket}) of
-	{AH, _Ack} ->
-	    Ack = ?IS_FLAG(AH#frame_header.flags, ?FLAG_ACK),
-	    lager:debug("~p", [Ack]),
-	    case http2_frame:read({Transport, Socket}) of
-		{_SSH, ServerSettings} ->
-		    ConnectionState = #connection_state{
-					 socket = {Transport, Socket},
-					 recv_settings = ClientSettings,
-					 send_settings = http2_frame_settings:overlay(#settings{}, ServerSettings)
-					},
-		    start_fsm(State#state{connection = ConnectionState});
-		Error ->
-		    Error
-	    end;
-	Error ->
-	    Error
-    end.
-
-start_fsm(#state{connection=#connection_state{socket={Transport,
-						      Socket}}}=State) ->
-    case gen_fsm:start_link(?MODULE, [State], []) of
-	{ok, Pid} ->
-	    Transport:controlling_process(Socket, Pid),
-	    {ok, Pid};
-	Error ->
-	    Error
-    end.
-
-client_options(Options) ->
-    ClientOptions = [binary, {packet, raw}, {active, false}],
-    case proplists:get_value(ssl, Options, false) of
-	false -> {gen_tcp, ClientOptions};
-	true ->
-	    SSLOptions = proplists:get_value(ssl_opts, Options, []),
-	    {ssl, ClientOptions ++ SSLOptions ++
-		 [{client_preferred_next_protocols, {client, [<<"h2">>]}}]}
-    end.
-
-process_binary(<<>>, #parser{frames=Frames}) ->
-    {Frames, #parser{frames = [], wfp = <<>>, wfh = undefined}};
-process_binary(<<HeaderBin:9/binary,Bin/binary>>,
-	       #parser{frames = Frames, wfp = <<>>,
-		       wfh = undefined} = Parser) ->
-    {Header, <<>>} = http2_frame:read_binary_frame_header(HeaderBin),
-    case byte_size(Bin) >= Header#frame_header.length of
-        true ->
-            {ok, Payload, Rem} = http2_frame:read_binary_payload(Bin, Header),
-	    process_binary(Rem, Parser#parser{
-				  frames = Frames ++ [{Header,Payload}]});
-        false ->
-	    {Frames, #parser{wfh = Header, wfp = Bin, frames = []}}
-    end;
-process_binary(Bin, #parser{wfh = Header, wfp = <<>>, 
-			    frames = Frames}=Parser) ->
-    case byte_size(Bin) >= Header#frame_header.length of
-        true ->
-            {ok, Payload, Rem} = http2_frame:read_binary_payload(Bin, Header),
-	    process_binary(Rem, #parser{
-				   wfh = undefined, wfp = <<>>,
-				   frames = Frames ++ [{Header,Payload}]});
-        false ->
-	    {Frames, Parser#parser{wfh = Header, wfp = Bin, frames = []}}
-    end;
-process_binary(Bin, #parser{wfp=Wfp}=Parser) ->
-    process_binary(iolist_to_binary([Wfp, Bin]), Parser#parser{wfp = <<>>}).
+frame_body(Body, #connection_state{send_settings=SS,
+				  next_available_stream_id=NASI}) ->
+    http2_frame_data:to_frames(NASI, Body, SS).
